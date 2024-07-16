7c8dc7a3fc16677ecc7f5d37f173706eb68411d0
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2024-07-16 17:41:10 +0700

    Update karena udah jago
    Update udah ada sertifikat ya kali ga di pamerin wkwk

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,119 +1,117 @@
 <template>
-
-    <div class="contact social-box">
-        <div class="container mt-4 mb-4 py-5">
-            <div class="row">
-                <div class="col-md-4 col-xs-12 text-center">
-                    <div class="box">
-                        <i class="fa fa-youtube fa-3x" aria-hidden="true"></i>
-                        <div class="box-title mt-2">
-                            <h3>Youtube</h3>
-                        </div>
-                        <div class="box-btn">
-                            <a href="https://www.youtube.com/@rifxslashsushi">Explore Now</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-xs-12  text-center">
-                    <div class="box">
-                        <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
-                        <div class="box-title mt-2">
-                            <h3>Twitter</h3>
-                        </div>
-                        <div class="box-btn">
-                            <a href="https://twitter.com/eexxvvn">Explore Now</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-xs-12 text-center">
-                    <div class="box">
-                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
-                        <div class="box-title mt-2">
-                            <h3>Facebook</h3>
-                        </div>
-                        <div class="box-btn">
-                            <a href="https://www.facebook.com/arif.alexander.56/">Explore Now</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-xs-12 text-center">
-                    <div class="box">
-                        <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
-                        <div class="box-title mt-2">
-                            <h3>Instagram</h3>
-                        </div>
-                        <div class="box-btn">
-                            <a href="https://www.instagram.com/eexxvvn/">Explore Now</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-xs-12 text-center">
-                    <div class="box">
-                        <i class="fa fa-github fa-3x" aria-hidden="true"></i>
-                        <div class="box-title mt-2">
-                            <h3>Github</h3>
-                        </div>
-                        <div class="box-btn">
-                            <a href="https://github.com/itsmebroarif">Explore Now</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-xs-12 text-center">
-                    <div class="box">
-                        <i class="fa fa-whatsapp fa-3x" aria-hidden="true"></i>
-                        <div class="box-title mt-2">
-                            <h3>Whatsapp</h3>
-                        </div>
-                        <div class="box-btn">
-                            <a
-                                href="https://whatsapp-form.vercel.app/">Explore
-                                Now</a>
-                        </div>
-                    </div>
-                </div>
-
+  <div class="contact social-box">
+    <div class="container mt-4 mb-4 py-5">
+      <div class="row">
+        <div class="col-md-4 col-xs-12 text-center">
+          <div class="box">
+            <i class="fa fa-youtube fa-3x" aria-hidden="true"></i>
+            <div class="box-title mt-2">
+              <h3>Youtube</h3>
             </div>
+            <div class="box-btn">
+              <a href="https://www.youtube.com/@rifxslashsushi">Explore Now</a>
+            </div>
+          </div>
         </div>
+
+        <div class="col-md-4 col-xs-12 text-center">
+          <div class="box">
+            <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+            <div class="box-title mt-2">
+              <h3>Twitter</h3>
+            </div>
+            <div class="box-btn">
+              <a href="https://twitter.com/eexxvvn">Explore Now</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12 text-center">
+          <div class="box">
+            <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+            <div class="box-title mt-2">
+              <h3>Facebook</h3>
+            </div>
+            <div class="box-btn">
+              <a href="https://www.facebook.com/arif.alexander.56/"
+                >Explore Now</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12 text-center">
+          <div class="box">
+            <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+            <div class="box-title mt-2">
+              <h3>Instagram</h3>
+            </div>
+            <div class="box-btn">
+              <a href="https://www.instagram.com/eexxvvn/">Explore Now</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12 text-center">
+          <div class="box">
+            <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+            <div class="box-title mt-2">
+              <h3>Github</h3>
+            </div>
+            <div class="box-btn">
+              <a href="https://github.com/itsmebroarif">Explore Now</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12 text-center">
+          <div class="box">
+            <i class="fa fa-whatsapp fa-3x" aria-hidden="true"></i>
+            <div class="box-title mt-2">
+              <h3>Whatsapp</h3>
+            </div>
+            <div class="box-btn">
+              <a href="https://whatsapp-form.vercel.app/">Explore Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style>
 body {
-    background: #d9ebee;
-    font-family: 'Poppins', sans-serif;
+  background: #d9ebee;
+  font-family: "Poppins", sans-serif;
 }
 
 .social-box .box {
-    background: #e8f1fc;
-    border-radius: 10px;
-    cursor: pointer;
-    margin: 20px 0;
-    padding: 40px 10px;
-    transition: all 0.5s ease-out;
+  background: #e8f1fc;
+  border-radius: 10px;
+  cursor: pointer;
+  margin: 20px 0;
+  padding: 40px 10px;
+  transition: all 0.5s ease-out;
 }
 
 .social-box .box:hover {
-    box-shadow: 0 0 6px #4183D7;
+  box-shadow: 0 0 6px #4183d7;
 }
 
 .social-box .box-text {
-    font-size: 15px;
-    line-height: 30px;
-    margin: 20px 0;
+  font-size: 15px;
+  line-height: 30px;
+  margin: 20px 0;
 }
 
 .social-box .box-btn a {
-    color: #000000;
-    font-size: 16px;
-    text-decoration: none;
+  color: #000000;
+  font-size: 16px;
+  text-decoration: none;
 }
 
 .social-box .fa {
-    color: #000000;
+  color: #000000;
 }
 </style>
--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,38 +1,194 @@
 <template>
     <div class="experience mt-4 py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 offset-md-3">
-                    <h4>Latest News</h4>
-                    <ul class="timeline">
-                        <li>
-                            <a href="http://kafeinarts.vercel.app/">KafeinArts</a> - 2023 
-                            <p> Currently I am a mentor and founder of Code With Arif, CWA is an online learning platform that provides various courses and training in the fields of computer science and information technology. This platform is usually available in PowerPoint or video conference which allows users to carry out teaching and learning activities in real-time and also get material in PowerPoint or portable document format.</p>
-                        </li>
-                        <li>
-                            <a href="http://arca-olive.vercel.app/">Arca Interactive</a> - 2022
-                            <p>As a talented website designer, you have had valuable experience in creating an attractive appearance and user experience at startup Arca Interactive. With expertise in designing layouts, graphics and visual elements, you have succeeded in creating an aesthetic and functional website. At Arca Interactive, you play a role in designing innovative website interfaces, ensuring that every web page you design meets high design standards and creates a compelling experience for users. With your dedication to website design, you have made a significant contribution to strengthening the brand identity and user experience at startup Arca Interactive.</p>
-                        </li>
-                        <li>
-                            <a href="https://www.guepedia.com/">Guepedia</a> - 2021
-                            <p>As a frontend developer, I have extensive experience in designing and developing attractive and responsive user interfaces. With expertise in HTML, CSS, and JavaScript with some framework like vue js for the javascript and bootstrap for the css framework, I have succeeded in creating engaging user experiences through engaging visual design and intuitive functionality. In every project, I always focus on creating an aesthetic appearance and optimal user experience.
-                            </p>
-                        </li>
-                    </ul>
-                </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h4>Work Experiences</h4>
+            <ul class="timeline">
+              <!-- Pengalaman Hallotrans -->
+              <li>
+                <a href="https://hallotrans.com/">Hallotrans</a> - 2024
+                <p>
+                  At Hallotrans, I serve as a dedicated fullstack web developer,
+                  leveraging my expertise in Laravel and Blade templating to
+                  spearhead the development of robust, scalable web applications.
+                  My role involves collaborating closely with cross-functional
+                  teams to translate business requirements into technical
+                  solutions. I am responsible for architecting backend services,
+                  designing efficient database schemas, and implementing
+                  responsive front-end interfaces that prioritize user experience
+                  and accessibility.
+                </p>
+                <p>
+                  I actively participate in the entire software development
+                  lifecycle, from initial concept and design to deployment and
+                  maintenance. I contribute to architectural decisions, ensuring
+                  our applications adhere to industry best practices and
+                  maintainable code standards. Through continuous integration and
+                  automated testing, I strive to deliver high-quality software
+                  solutions that meet performance metrics and exceed stakeholder
+                  expectations.
+                </p>
+                <p>
+                  Beyond technical implementation, I play a key role in optimizing
+                  system performance and troubleshooting complex issues to ensure
+                  seamless functionality across different devices and browsers. I
+                  collaborate with UX/UI designers to create visually appealing
+                  interfaces that enhance usability and drive engagement.
+                  Additionally, I integrate third-party APIs and services to
+                  extend application functionality and improve user workflows.
+                </p>
+                <p>
+                  My experience at Hallotrans has sharpened my skills in
+                  problem-solving, project management, and team collaboration. I
+                  thrive in dynamic environments where innovation and continuous
+                  learning are encouraged, allowing me to stay abreast of emerging
+                  technologies and industry trends. By fostering a culture of
+                  transparency and knowledge sharing, I contribute to a cohesive
+                  team environment that values creativity and technical
+                  excellence.
+                </p>
+              </li>
+              <!-- Pengalaman KafeinArts -->
+              <li>
+                <a href="http://kafeinarts.vercel.app/">KafeinArts</a> - 2023
+                <p>
+                  I am a mentor and founder of Code With Arif, where I oversee
+                  online learning platforms and courses in computer science and
+                  IT. At Code With Arif, my role extends beyond technical
+                  instruction; I am committed to cultivating a supportive learning
+                  environment where aspiring developers can gain practical skills
+                  and industry insights. Through personalized mentorship and
+                  comprehensive course materials, I empower students to tackle
+                  complex coding challenges and stay abreast of cutting-edge
+                  technologies. Our platform fosters a community of learners
+                  dedicated to professional growth and innovation in the
+                  ever-evolving field of technology.
+                </p>
+              </li>
+              <!-- Pengalaman Arca Interactive -->
+              <li>
+                <a href="http://arca-olive.vercel.app/">Arca Interactive</a> -
+                2022
+                <p>
+                  At Arca Interactive, I specialized in designing aesthetic and
+                  functional websites, contributing significantly to enhancing
+                  brand identity and improving user experience. My role
+                  encompassed leading the design process from concept to
+                  execution, collaborating closely with stakeholders to understand
+                  brand objectives and user needs. By implementing best practices
+                  in UX/UI design, I ensured that every website not only met but
+                  exceeded industry standards for usability and visual appeal.
+                </p>
+                <p>
+                  Throughout my tenure, I championed a user-centered design
+                  approach, conducting thorough research and usability testing to
+                  iterate and refine interface designs. This iterative process not
+                  only optimized user workflows but also increased user engagement
+                  metrics, contributing directly to the company's growth and
+                  client satisfaction. I also played a pivotal role in
+                  cross-functional teams, translating design concepts into
+                  functional specifications for developers and maintaining design
+                  integrity throughout implementation phases.
+                </p>
+                <p>
+                  Beyond design, I contributed to strategic planning sessions,
+                  offering insights on emerging design trends and technologies to
+                  drive innovation within the organization. I collaborated closely
+                  with marketing and content teams to align web design efforts
+                  with broader digital strategies, ensuring cohesive brand
+                  messaging and seamless user journeys across all platforms.
+                </p>
+                <p>
+                  My experience at Arca Interactive sharpened my skills in project
+                  management, team collaboration, and client communication,
+                  enabling me to deliver projects on time and within budget while
+                  exceeding client expectations. I am proud to have contributed to
+                  establishing Arca Interactive as a leader in providing
+                  aesthetically pleasing, user-friendly websites that resonate
+                  with target audiences and elevate brand perceptions.
+                </p>
+              </li>
+              <!-- Pengalaman Guepedia -->
+              <li>
+                <a href="https://www.guepedia.com/">Guepedia</a> - 2021
+                <p>
+                  As a frontend developer at Guepedia, I specialized in creating
+                  captivating user interfaces using a robust stack of technologies
+                  including HTML, CSS, JavaScript, Vue.js, Laravel, and Bootstrap.
+                  My role centered around translating design concepts into
+                  interactive and responsive web applications that optimized user
+                  engagement and satisfaction.
+                </p>
+                <p>
+                  I leveraged my expertise in HTML and CSS to craft visually
+                  appealing layouts and styles, ensuring consistency across
+                  different browsers and devices. JavaScript and Vue.js were
+                  instrumental in implementing dynamic and interactive components,
+                  enhancing user interactions and usability. Additionally,
+                  integrating Laravel for backend services allowed me to build
+                  seamless data-driven applications, ensuring efficient
+                  communication between frontend and backend systems.
+                </p>
+                <p>
+                  Throughout my tenure at Guepedia, I actively participated in
+                  agile development methodologies, collaborating closely with
+                  designers and backend developers to deliver cohesive and
+                  scalable solutions. I conducted thorough testing and
+                  optimization to maintain high performance and usability
+                  standards, continuously refining features based on user feedback
+                  and analytics data.
+                </p>
+                <p>
+                  My experience at Guepedia strengthened my proficiency in
+                  frontend technologies and frameworks, equipping me with a deep
+                  understanding of user-centric design principles and
+                  cross-functional teamwork. I am passionate about creating
+                  impactful digital experiences that not only meet but exceed user
+                  expectations, driving business growth and customer satisfaction.
+                </p>
+              </li>
+            </ul>
+          </div>
+          <!-- Tampilan sertifikat -->
+          <div class="col-md-4">
+            <div class="certificate">
+              <h3>Certificate And Reward</h3>
+              <ul>
+                <li>
+                  <strong>Web Design Certification</strong>
+                  <p>
+                    Awarded for proficiency in designing aesthetically pleasing
+                    and functional websites, demonstrating expertise in UX/UI
+                    design principles and industry best practices.
+                </p>
+                <a href="img/certificates/web-design.jpeg">View Certificate</a>
+            </li>
+            <li>
+                <strong>JavaScript Algorithm and Data Structure Certification</strong>
+                <p>
+                    Recognized for mastery in JavaScript fundamentals, data
+                    structures, and algorithms, showcasing ability to solve
+                    complex problems and optimize code efficiency.
+                </p>
+                <a href="img\certificates\javascript-data-structure.jpeg">View Certificate</a>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</template>
-
-<style>
-ul.timeline {
+  </template>
+  
+  <style>
+  ul.timeline {
     list-style-type: none;
     position: relative;
-}
-
-ul.timeline:before {
-    content: ' ';
+  }
+  
+  ul.timeline:before {
+    content: " ";
     background: #d4d9df;
     display: inline-block;
     position: absolute;
@@ -40,15 +196,15 @@ ul.timeline:before {
     width: 2px;
     height: 100%;
     z-index: 400;
-}
-
-ul.timeline>li {
+  }
+  
+  ul.timeline > li {
     margin: 20px 0;
     padding-left: 20px;
-}
-
-ul.timeline>li:before {
-    content: ' ';
+  }
+  
+  ul.timeline > li:before {
+    content: " ";
     background: white;
     display: inline-block;
     position: absolute;
@@ -58,5 +214,38 @@ ul.timeline>li:before {
     width: 20px;
     height: 20px;
     z-index: 400;
-}
-</style>
+  }
+  
+  .certificate {
+    background-color: #f9f9f9;
+    padding: 20px;
+    border: 1px solid #e0e0e0;
+    border-radius: 8px;
+  }
+  
+  .certificate h4 {
+    font-size: 20px;
+    margin-bottom: 15px;
+  }
+  
+  .certificate ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  .certificate ul li {
+    margin-bottom: 15px;
+  }
+  
+  .certificate ul li strong {
+    display: block;
+    font-size: 16px;
+    margin-bottom: 5px;
+  }
+  
+  .certificate ul li p {
+    font-size: 14px;
+    line-height: 1.6;
+  }
+  </style>
+  
